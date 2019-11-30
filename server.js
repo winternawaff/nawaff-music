bd4de902a9b0dc432d6854732648dcdba4f09f38
winternawaff <57063193+winternawaff@users.noreply.github.com>
2019-11-30 13:38:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html'); 
+});
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+const http = require('http');
+app.get("/", (request, response) => {
+  response.sendStatus(200); 
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://project.glitch.me`);
+}, 280000); 
+
+var shell = require('shelljs');
+  
+shell.exec('java -jar JMusicBot-0.2.6.jar')
